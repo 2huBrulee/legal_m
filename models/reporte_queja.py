a4074839dda811cb590c3352969236dae56cac22
Alejandro Merino <alejandro2hpxa@gmail.com>
2018-11-23 10:41:25 -0500

    redo

--- models/reporte_queja.py
+++ models/reporte_queja.py
@@ -4,6 +4,5 @@ from odoo import models, fields, api
 
 class ReporteQuejaInherited(models.Model):
 	_inherit = 'reporte.queja'
-    _name = 'legal_m.queja'
 
 	tomado = fields.Char(string='tomado', size=64)
