6af1f0b15b2c8cccba00bea6e5dc8637a28cef37
Alejandro Merino <alejandro2hpxa@gmail.com>
2018-11-23 14:50:30 -0500

    redo

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/reportequeja_view.xml',
+        'views/legal_m_queja_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/reporte_queja.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ReporteQuejaInherited(models.Model):
-	_inherit = 'reporte.queja'
-
-	tomado = fields.Char(string='tomado', size=64)
